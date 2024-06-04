2ab3f32100ca50b259c57d3a4552f191ffa76872
Vladyslav Poliakov <vladon24121995@gmail.com>
2024-06-04 22:55:02 +0300

    Load data

---
+++ src/pages/SignInPage/ui/SignInForm/SignInFormData.ts
@@ -0,0 +1,14 @@
+export const SignInInputs = [
+  {
+    name: 'email',
+    placeholder: 'example@gmail.com',
+    label: 'Email',
+    type: 'email',
+  },
+  {
+    name: 'password',
+    placeholder: '********',
+    label: 'Password',
+    type: 'password',
+  },
+];
