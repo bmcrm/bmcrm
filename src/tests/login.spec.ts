5457aa73fa3e300b75b4c92d1cbd9ef941689cc8
Sergey <sergsmile29@gmail.com>
2024-06-07 00:40:55 +0300

    create login test

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -1,10 +1,10 @@
-// tests/login.spec.ts
 import { test, expect } from '@playwright/test';
+import { EnvConfigs } from 'shared/config/env/env';
 
 test('successful login', async ({ page }) => {
   await page.goto('https://app.dev.bmcrm.camp/login');
-  await page.fill('input[name="email"]', 's.drynkin@karmafixer.com');
-  await page.fill('input[name="password"]', '123qweasdQ!');
+  await page.fill('input[name="email"]', EnvConfigs.TEST_ACCOUNT_TCO_EMAIL);
+  await page.fill('input[name="password"]', EnvConfigs.TEST_ACCOUNT_TCO_PASSWORD);
   await page.click('button[type="submit"]');
   await expect(page).toHaveURL('https://app.dev.bmcrm.camp/login');
   await expect(page.locator('text=Leads')).toBeVisible();
