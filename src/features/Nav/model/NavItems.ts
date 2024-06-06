0ab06504d7344c50d2c718d2a6e3487642a1a2b3
Vladyslav Poliakov <vladon24121995@gmail.com>
2024-06-06 16:53:34 +0300

    fix: grayout for nav and setting, refactor from cognito.ts to userAuth.ts

--- src/features/Nav/model/NavItems.ts
+++ src/features/Nav/model/NavItems.ts
@@ -3,27 +3,32 @@ import { RoutePath } from 'app/providers/AppRouter';
 export type NavItemType = {
   path: string;
   text: string;
+  disabled?: boolean;
 };
 
 export const NavItemsList: NavItemType[] = [
-  // {
-  //   path: RoutePath.dashboard,
-  //   text: 'Dashboard',
-  // },
+  {
+    path: RoutePath.dashboard,
+    text: 'Dashboard',
+    disabled: true,
+  },
   {
     path: RoutePath.funnel,
     text: 'Funnel',
   },
-  // {
-  //   path: RoutePath.finances,
-  //   text: 'Finances',
-  // },
-  // {
-  //   path: RoutePath.shifts,
-  //   text: 'Shifts',
-  // },
-  // {
-  //   path: RoutePath.inventory,
-  //   text: 'Inventory',
-  // },
+  {
+    path: RoutePath.finances,
+    text: 'Finances',
+    disabled: true,
+  },
+  {
+    path: RoutePath.shifts,
+    text: 'Shifts',
+    disabled: true,
+  },
+  {
+    path: RoutePath.inventory,
+    text: 'Inventory',
+    disabled: true,
+  },
 ];
