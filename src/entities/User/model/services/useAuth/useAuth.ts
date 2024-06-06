3e4749460111b1bbb2e65bb3deeafd96c3604532
Sergey <sergsmile29@gmail.com>
2024-06-06 17:21:42 +0300

    Revert "refactor: include register to zustand"
    This reverts commit dff73e0187cdbc13ee9f8eb2d4c518faa42b2faa.

--- src/entities/User/model/services/useAuth/useAuth.ts
+++ src/entities/User/model/services/useAuth/useAuth.ts
@@ -1,19 +1,17 @@
-import { confirmEmail, loginUser, signUpUser } from 'shared/api/userAuth/userAuth';
+import { confirmEmail, loginUser } from 'shared/api/userAuth/userAuth';
 import { create } from 'zustand';
 import { createJSONStorage, devtools, persist } from 'zustand/middleware';
 import { ConfirmSignUpCommandOutput } from '@aws-sdk/client-cognito-identity-provider';
-import { type IInputsData } from 'entities/User';
 
 interface AuthState {
   isLoggedIn: boolean;
+  login: (values: { email: string; password: string }) => Promise<void>;
   accessToken: string;
+  logout: () => void;
   idToken: string;
   refreshToken: string;
   isLoading: boolean;
-  login: (values: { email: string; password: string }) => Promise<void>;
   confirm: (data: ConfirmTypes) => Promise<ConfirmSignUpCommandOutput>;
-  register: (credentials: IInputsData) => Promise<void>;
-  logout: () => void;
 }
 
 export interface IResponse {
@@ -23,7 +21,7 @@ export interface IResponse {
 }
 
 type ConfirmTypes = {
-  code: string;
+  code: string,
   email: string;
 };
 
@@ -56,19 +54,8 @@ const useAuth = create<AuthState>()(
             set({ isLoading: false });
           }
         },
-        register: async credentials => {
-          try {
-            set({ isLoading: true });
-            signUpUser(credentials);
-          } catch (error) {
-            console.error(error);
-            throw error;
-          } finally {
-            set({ isLoading: false });
-          }
-        },
         logout: () => set({ isLoggedIn: false, accessToken: '', idToken: '', refreshToken: '' }),
-        confirm: async data => {
+        confirm: async data  => {
           try {
             set({ isLoading: true });
             const response = await confirmEmail(data);
--- src/pages/SignUpOwnerPage/ui/SignUpOwnerPage/SignUpOwnerPage.tsx
+++ src/pages/SignUpOwnerPage/ui/SignUpOwnerPage/SignUpOwnerPage.tsx
@@ -1,42 +1,38 @@
 import { memo, useCallback } from 'react';
 import toast from 'react-hot-toast';
+import { signUpUser } from 'shared/api/userAuth/userAuth.ts';
 
-import { type IInputsData, OwnerSignUpForm, useAuth } from 'entities/User';
+import { type IInputsData, OwnerSignUpForm } from 'entities/User';
 import AuthPageTemplate from 'shared/ui/AuthPageTemplate/AuthPageTemplate';
 import AuthFormTemplate from 'shared/ui/AuthFormTemplate/AuthFormTemplate';
 import { useNavigate } from 'react-router-dom';
 import { RoutePath } from 'app/providers/AppRouter';
-import FormLoader from 'features/FormLoader';
 
 const SignUpOwnerPage = memo(() => {
-  const { register, isLoading } = useAuth(state => ({
-    register: state.register,
-    isLoading: state.isLoading,
-  }));
   const navigate = useNavigate();
   const handleSubmit = useCallback(
     (values: IInputsData, { resetForm }: { resetForm: () => void }) => {
       const credentials = {
         ...values,
       };
-      register(credentials)
+      signUpUser(credentials)
         .then(() => {
           toast.success('Sign-up successful!', { duration: 2000, position: 'top-right' });
           resetForm();
           navigate(RoutePath.sign_in, { replace: true });
         })
-        .catch(() => {
+        .catch(error => {
           toast.error('User already exist!', { duration: 2000, position: 'top-right' });
+          console.error('Sign-up failed:', error);
         });
     },
-    [navigate, register]
+    [navigate]
   );
 
   return (
     <AuthPageTemplate>
       <AuthFormTemplate badge={'Create a camp and account'}>
         <OwnerSignUpForm handleSubmit={handleSubmit} />
-        {isLoading && <FormLoader />}
       </AuthFormTemplate>
     </AuthPageTemplate>
   );
