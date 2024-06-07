eafeb402264a03457d0b76e4a529def439b7c4ed
alex-roman-kfx <alexroman@karmafixer.com>
2024-06-07 07:50:54 -0400

    playwright cicd WIP

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -1,11 +1,24 @@
 import { test, expect } from '@playwright/test';
-import { EnvConfigs } from 'shared/config/env/env';
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
+
+// Initialize AWS SSM client to pull login credentials of the testuser
+const client = new SSMClient({ region: 'us-east-1' });
+function getParameter(name: string): Promise<string> {
+  const resp = client.send(new GetParameterCommand({ Name: name }));
+  return resp.then(r => r.Parameter?.Value || '');
+}
+
+const LOGIN_URL = 'https://app.dev.bmcrm.camp/login';
+
+// we have separate test account for a TCO persona and a Camper persona
+const TEST_ACCOUNT_TCO_EMAIL = await getParameter('/webapp/test/tco_email');
+const TEST_ACCOUNT_TCO_PASSWORD = await getParameter('/webapp/test/tco_password');
 
 test('successful login', async ({ page }) => {
-  await page.goto('https://app.dev.bmcrm.camp/login');
-  await page.fill('input[name="email"]', EnvConfigs.TEST_ACCOUNT_TCO_EMAIL);
-  await page.fill('input[name="password"]', EnvConfigs.TEST_ACCOUNT_TCO_PASSWORD);
+  await page.goto(LOGIN_URL);
+  await page.fill('input[name="email"]', TEST_ACCOUNT_TCO_EMAIL);
+  await page.fill('input[name="password"]', TEST_ACCOUNT_TCO_PASSWORD);
   await page.click('button[type="submit"]');
-  await expect(page).toHaveURL('https://app.dev.bmcrm.camp/login');
+  await expect(page).toHaveURL(LOGIN_URL);
   await expect(page.locator('text=Leads')).toBeVisible();
 });
