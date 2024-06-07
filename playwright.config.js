5943f97972e328d98410f175f169c3b7e33cd940
Sergey <sergsmile29@gmail.com>
2024-06-08 00:09:38 +0300

    update config

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
-  testDir: 'tests',
+  testDir: 'src/tests',
 
   // Run all tests in parallel.
   fullyParallel: true,
@@ -22,7 +22,7 @@ export default defineConfig({
 
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:5173/',
 
     // Collect trace when retrying the failed test.
     trace: 'on-first-retry',
@@ -37,7 +37,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests.
   webServer: {
     command: 'npm run dev',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://localhost:5173/',
     reuseExistingServer: !process.env.CI,
   },
 });
