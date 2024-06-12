8dbbd9014e895851068debceca7f2837c332542a
Sergey <sergsmile29@gmail.com>
2024-06-13 02:40:39 +0300

    add-methods-get-patch-camper

--- src/entities/Camper/model/services/useCampers/useCampers.ts
+++ src/entities/Camper/model/services/useCampers/useCampers.ts
@@ -9,8 +9,11 @@ interface CamperState {
   isLoading: boolean;
   campers: ICamper[];
   isError: string | null;
+  camper: ICamper;
   getCampers(): Promise<void>;
+  getCamper(email: string, camp_id: string): Promise<void>;
   getCamperById(id: string): Promise<ICamper>;
+  updateCamper(email: string, camp_id: string, data: Partial<ICamper>): Promise<void>;
 }
 
 const useCampers = create<CamperState>()(
@@ -18,7 +21,7 @@ const useCampers = create<CamperState>()(
     isLoading: false,
     isError: null,
     campers: [],
-
+    camper: {} as ICamper,
     getCampers: async () => {
       try {
         set({ isLoading: true });
@@ -35,6 +38,40 @@ const useCampers = create<CamperState>()(
         set({ isLoading: false });
       }
     },
+    getCamper: async (email: string, camp_id: string) => {
+      try {
+        set({ isLoading: true });
+        const response = await axios.get(`https://campers.dev.bmcrm.camp/campers/${camp_id}/${email}`, {
+          headers: {
+            Authorization: useAuth.getState().idToken,
+          },
+        });
+
+        set({ isLoading: false, camper: response?.data[0] || null });
+      } catch (error) {
+        throw new Error('Error fetching campers: ' + error);
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+    updateCamper: async (email: string, camp_id: string, data: Partial<ICamper>) => {
+      try {
+        set({ isLoading: true });
+        const response = await axios.patch('https://campers.dev.bmcrm.camp/campers', {
+          headers: {
+            Authorization: useAuth.getState().idToken,
+          },
+          body: { ...data, email: email, camp_id: camp_id },
+        });
+
+        set({ isLoading: false, campers: response.data });
+      } catch (error) {
+        throw new Error('Error fetching campers: ' + error);
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+
     getCamperById: async (id: string) => {
       try {
         set({ isLoading: true });
