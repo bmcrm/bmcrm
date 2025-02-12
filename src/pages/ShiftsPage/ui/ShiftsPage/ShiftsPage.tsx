09563d99da39329ba7e501a308d96429e114247b
Vlad <vladyslav.poliakov.work@gmail.com>
2025-02-12 15:58:53 +0200

    feat(EmptyShifts state): done

--- src/pages/ShiftsPage/ui/ShiftsPage/ShiftsPage.tsx
+++ src/pages/ShiftsPage/ui/ShiftsPage/ShiftsPage.tsx
@@ -3,10 +3,12 @@ import { useToggle } from '@shared/hooks/useToggle';
 import { Container } from '@shared/ui/Container';
 import { Button } from '@shared/ui/Button';
 import { Loader } from '@shared/ui/Loader';
+import { Image } from '@shared/ui/Image';
 import { ShiftModal } from '@widgets/ShiftModal';
 import { ShiftsList } from '../ShiftsList/ShiftsList';
 import { useGetShifts, ShiftFormTheme, type IShift } from '@entities/Shift';
 import styles from './ShiftsPage.module.scss';
+import EmptyImg from '@shared/assets/images/shifts/not-found.png';
 
 const ShiftsPage = () => {
   const [currentFormTheme, setCurrentFormTheme] = useState<ShiftFormTheme>(ShiftFormTheme.CREATE);
@@ -29,10 +31,17 @@ const ShiftsPage = () => {
   return (
     <>
       <section className={styles.shifts}>
-        <Container fluid>
+        <Container className={styles.shifts__container} fluid>
           <Button className={'ml-a'} onClick={handleCreateShift}>Add new</Button>
           {isLoading && <Loader className={styles.loader} />}
-          {shifts && <ShiftsList className={'mt-40'} shifts={shifts} onEditShift={handleEditShift} />}
+          {shifts && shifts.length > 0 ? (
+            <ShiftsList className={'mt-40'} shifts={shifts} onEditShift={handleEditShift} />
+          ) : (
+            <div className={styles.shifts__empty}>
+              <Image src={EmptyImg} maxWidth={400} />
+              <h2>No shifts found</h2>
+            </div>
+          )}
         </Container>
       </section>
       <ShiftModal isOpen={isOpen} onClose={close} theme={currentFormTheme} currentShift={currentShift} />
