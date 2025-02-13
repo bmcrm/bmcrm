09f04cae24721d61541864e5079f902f2df0c962
Vlad <vladyslav.poliakov.work@gmail.com>
2025-02-13 20:25:26 +0200

    feat(tests): in progress

--- src/__tests__/global/auth.spec.ts
+++ src/__tests__/global/auth.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
-import { deleteUser, getTestParameters, getURLs, login } from '@shared/tests/utils/utils';
+import { deleteUser, getParameter, getTestParameters, getURLs, login } from '@shared/tests/utils/utils';
 
+let URLS: Record<string, string>;
 let TEST_PARAMS: {
   TEST_COGNITO_POOL_ID: string;
   COGNITO_APP_CLIENT_ID: string;
@@ -8,24 +9,40 @@ let TEST_PARAMS: {
   TEMP_PASSWORD: string;
   NEW_PASSWORD: string;
 };
-let URLS: Record<string, string>;
+let CAMPERS_TABLE: string;
 
 test.describe('Testing login and register flow', () => {
 
   test.beforeAll(async () => {
-    TEST_PARAMS = await getTestParameters();
-    URLS = await getURLs();
+    [
+      URLS,
+      TEST_PARAMS,
+      CAMPERS_TABLE,
+    ] = await Promise.all([
+      getURLs(),
+      getTestParameters(),
+      getParameter('/campers/ddb_table_name'),
+    ]);
   });
 
-  test.skip('successful register', async ({ page }) => {
+  test('successful register', async ({ page }) => {
     await page.goto(URLS.REGISTER_URL);
-    await page.fill('input[name="name"]', 'First Name');
-    await page.fill('input[name="SOID"]', 'mySOID');
-    await page.fill('input[name="email"]', 'test_email@test.com');
+    await page.fill('input[name="camp_name"]', 'fake-camp-test');
+    await page.fill('input[name="city"]', 'city');
+    await page.fill('input[name="first_name"]', 'first name');
+    await page.fill('input[name="last_name"]', 'last_name');
+    await page.fill('input[name="playa_name"]', 'playa name');
+    await page.fill('input[name="email"]', 'fake@email.com');
     await page.fill('input[name="password"]', 'Password1!');
+    await page.click('label[aria-label="Accept terms"]');
     await page.click('button[type="submit"]');
-    await expect(page.locator('h1')).toHaveText('Confirmation');
-    await deleteUser(TEST_PARAMS.TEST_COGNITO_POOL_ID, 'test_email@test.com');
+    await expect(page.locator('h1')).toHaveText('Account Verification');
+    await deleteUser({
+      cognitoPoolId: TEST_PARAMS.TEST_COGNITO_POOL_ID,
+      email: 'fake@email.com',
+      campID: 'fake-camp-test',
+      tableName: CAMPERS_TABLE,
+    });
   });
 
   test('successful login', async ({ page }) => {
